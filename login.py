2b3669120a3cdb6097519da15b0993b2fdf01042
Suraj Tiwari <surajtiwarideveloper@gmail.com>
2025-12-13 10:55:53 +0530

    Login_system.py

---
+++ login.py
@@ -0,0 +1,22 @@
+print("                              Login System                     ")
+print()
+print()
+print()
+count = 0
+while  count <3 :
+     user_input = "xyz"
+     user_password ="887972"
+     username = input("enter name")
+     password = input("Enter password")
+    
+     if username == user_input and user_password == password :
+                     print("Access Granted")
+                     break
+     else :
+                     print("Try Again ")
+                     count += 1
+ 
+if count == 2 :
+     print("Number of attempts exhausted")
+     def ramesh(x,y) :
+          
